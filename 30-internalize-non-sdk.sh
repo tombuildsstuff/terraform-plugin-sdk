4e95285a38c1db27262df24494cf66a11cde9849
Radek Simko <radek.simko@gmail.com>
2019-08-22 07:09:58 +0100

    avoid moving vendor folder

--- 30-internalize-non-sdk.sh
+++ 30-internalize-non-sdk.sh
@@ -48,7 +48,8 @@ echo "SDK packages stored in $SDK_PKGS_LIST_PATH"
 
 SDK_FOLDERS_PATTERNS_PATH=$(mktemp)
 cat $SDK_PKGS_LIST_PATH | xargs -I{} sh -c "echo ^{}\$; echo ^{}/testdata" > $SDK_FOLDERS_PATTERNS_PATH
-NONSDK_FOLDERS=$(find . -type d -and \( ! -path './.git*' \) | grep -xFv '.' | grep -v -f $SDK_FOLDERS_PATTERNS_PATH)
+NONSDK_FOLDERS=$(find . -type d -and \( ! -path './.git*' \) -and \( ! -path './vendor*' \) -and \( ! -path './go.*' \) | grep -xFv '.' | grep -v -f $SDK_FOLDERS_PATTERNS_PATH)
+NONSDK_FOLDERS="$NONSDK_FOLDERS"
 NONSDK_GO_PKGS=$(go list -json ./... | jq -r .ImportPath | sed -e 's/^github.com\/hashicorp\/terraform-plugin-sdk\/./' | grep -v -f $SDK_FOLDERS_PATTERNS_PATH | sed -e 's/^\.\///')
 
 NONSDK_GO_PKGS_PATH=$(mktemp)
